b6c004ed3491ff94eaecf8663f0b32703573a71e
Gabriel <gabie00018@gmail.com>
2025-10-14 22:30:30 -0300

    cleanup

---
+++ physics.h
@@ -0,0 +1,44 @@
+#include "tinyphysicsengine.h"
+
+#define FPS 60
+
+#define ACC ((25 * 30) / FPS )
+
+#define JOINT_SIZE (TPE_F / 4)
+#define BALL_SIZE (5 * TPE_F / 4)
+
+#define HEIGHTMAP_3D_RESOLUTION 8
+#define HEIGHTMAP_3D_STEP (TPE_F * 2)
+
+#define ROOM_SIZE (HEIGHTMAP_3D_RESOLUTION * HEIGHTMAP_3D_STEP + JOINT_SIZE)
+
+#define HEIGHTMAP_3D_POINTS (HEIGHTMAP_3D_RESOLUTION * HEIGHTMAP_3D_RESOLUTION)
+#define WATER_JOINTS (HEIGHTMAP_3D_RESOLUTION * HEIGHTMAP_3D_RESOLUTION)
+#define WATER_CONNECTIONS (2 * ((HEIGHTMAP_3D_RESOLUTION - 1) * HEIGHTMAP_3D_RESOLUTION))
+
+#define SCALE_3D 0.001f
+
+#define MAX_BODIES 128
+#define MAX_JOINTS 1024
+#define MAX_CONNECTIONS 2048
+
+TPE_World tpe_world;
+TPE_Body tpe_bodies[MAX_BODIES];
+TPE_Joint tpe_joints[MAX_JOINTS];
+TPE_Connection tpe_connections[MAX_CONNECTIONS];
+
+TPE_Joint joints[WATER_JOINTS + MAX_BODIES - 1];
+TPE_Connection connections[WATER_CONNECTIONS];
+TPE_Body bodies[MAX_BODIES];
+
+TPE_Vec3 helper_heightmapPointLocation(int index)
+{
+    return TPE_vec3(
+        (-1 * HEIGHTMAP_3D_RESOLUTION * HEIGHTMAP_3D_STEP) / 2 + (index % HEIGHTMAP_3D_RESOLUTION) * HEIGHTMAP_3D_STEP + HEIGHTMAP_3D_STEP / 2,0,
+        (-1 * HEIGHTMAP_3D_RESOLUTION * HEIGHTMAP_3D_STEP) / 2 + (index / HEIGHTMAP_3D_RESOLUTION) * HEIGHTMAP_3D_STEP + HEIGHTMAP_3D_STEP / 2);
+}
+
+TPE_Vec3 environmentDistance(TPE_Vec3 p, TPE_Unit maxD)
+{
+    return TPE_envAABoxInside(p,TPE_vec3(0,0,0),TPE_vec3(ROOM_SIZE,ROOM_SIZE,ROOM_SIZE));
+}
